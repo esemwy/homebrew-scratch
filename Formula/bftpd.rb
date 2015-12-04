0f131e23aa28c9ab9885b6de5eed7951e363aa03
Esemwy <github.5.smy@neverbox.com>
2015-12-04 14:29:03 -0500

    Add post-stor-script.sh

--- Formula/bftpd.rb
+++ Formula/bftpd.rb
@@ -4,8 +4,7 @@ class Bftpd < Formula
   url "http://tcpdiag.dl.sourceforge.net/project/bftpd/bftpd/bftpd-4.4/bftpd-4.4.tar.gz"
   sha256 "b805ebbdd3de993ca14d733ad9ad007ac342a1646b257d91a53ec87a36c7f741"
 
-  # Patch to remove UTMPX dependency, locate OS X's PAM library, and
-  #   remove incompatible LDFLAGS. (reported to developer via email)
+  # Patch to fix UTMPX dependency
   patch :DATA
 
   def install
@@ -13,13 +12,36 @@ class Bftpd < Formula
     system "make"
     inreplace "bftpd.conf", 'PORT="21"', 'PORT="2021"'
     inreplace "bftpd.conf", 'DENY_LOGIN="Anonymous', '#DENY_LOGIN="Anonymous'
-    inreplace "bftpd.conf", '/var/log', "#{var}/log"
+    inreplace "bftpd.conf", '/var', var
     inreplace "bftpd.conf", '/etc', etc
+    inreplace "bftpd.conf", 'AUTH="PASSWD"', 'AUTH="PAM"'
 
     # make install has all the paths hardcoded; this is easier:
     sbin.install "bftpd"
     etc.install "bftpd.conf"
     man8.install "bftpd.8"
+
+    script = bin/"post-stor-script.conf"
+    unless script.exist?
+      script.write <<-EOS.undent
+        #!/bin/bash
+        
+        ARGUMENT=$1
+        if [[ "$ARGUMENT" == *.JPG ]]
+        then
+            DIR=$(dirname "$ARGUMENT")
+            BASE=$(basename "$ARGUMENT" .JPG)
+            ID=${BASE:4}
+            OUTPUT="${DIR}/HPC_${ID}.JPG"
+            convert -geometry 1800x1200 "$ARGUMENT" -gravity SouthEast \
+                -stroke '#000C' -strokewidth 2 -annotate 0 $ID \
+                -stroke  none   -fill yellow -annotate 0 $ID "$OUTPUT"
+            open "$OUTPUT"
+        else
+            echo "Ignoring '$ARGUMENT'"
+        fi
+      EOS
+    chmod 0755, script
   end
 end
 
