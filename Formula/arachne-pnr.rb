1941e9319a1e04188f0a4c7c76d3dd8be033524e
Esemwy <github.5.smy@neverbox.com>
2018-05-16 14:46:54 -0400

    Patch arachne-pnr

--- Formula/arachne-pnr.rb
+++ Formula/arachne-pnr.rb
@@ -10,6 +10,7 @@ class ArachnePnr < Formula
 
   def install
     ENV.prepend_path "PKG_CONFIG_PATH", "/usr/local/lib/pkgconfig"
+    system "make", "PREFIX=#{prefix}"
     system "make", "install", "PREFIX=#{prefix}"
   end
 
