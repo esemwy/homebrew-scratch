1dc3e461e45a4f6765507269d0fbb8125ff608f0
Esemwy <github.5.smy@neverbox.com>
2018-05-16 14:34:00 -0400

    Patch arachne-pnr

--- Formula/arachne-pnr.rb
+++ Formula/arachne-pnr.rb
@@ -1,10 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ArachnePnr < Formula
   desc "Arachne-pnr implements the place and route step of the hardware compilation process for FPGAs."
   homepage "https://github.com/cseed/arachne-pnr/"
   head "https://github.com/cseed/arachne-pnr"
+  sha256 "b9ad8a6a57d72cd9d90736b5c9bc83960d79b667ab8c14f60a704519f41d7785"
 
   depends_on "python3"
   depends_on "pkg-config"
