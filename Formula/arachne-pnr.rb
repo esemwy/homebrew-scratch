ae343b9932828bbe46a22a19a6e6ff6ea049a0ab
Esemwy <github.5.smy@neverbox.com>
2018-05-16 14:42:54 -0400

    Patch arachne-pnr

--- Formula/arachne-pnr.rb
+++ Formula/arachne-pnr.rb
@@ -1,7 +1,7 @@
-class Arachnepnr < Formula
+class ArachnePnr < Formula
   desc "Arachne-pnr implements the place and route step of the hardware compilation process for FPGAs."
   homepage "https://github.com/cseed/arachne-pnr/"
-  head "https://github.com/cseed/arachne-pnr"
+  head "https://github.com/cseed/arachne-pnr.git"
   sha256 "b9ad8a6a57d72cd9d90736b5c9bc83960d79b667ab8c14f60a704519f41d7785"
 
   depends_on "python3"
