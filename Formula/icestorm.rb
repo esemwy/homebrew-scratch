19a643fe4afa88d53b595813d9a7a7ceefa247b7
Esemwy <github.5.smy@neverbox.com>
2018-05-17 19:03:56 -0400

    Fix head-only

--- Formula/icestorm.rb
+++ Formula/icestorm.rb
@@ -2,6 +2,8 @@ class Icestorm < Formula
   desc "Project IceStorm aims at reverse engineering and documenting the bitstream format of Lattice iCE40 FPGAs"
   homepage "http://www.clifford.at/icestorm/"
   head "https://github.com/cliffordwolf/icestorm.git"
+  url "https://github.com/cliffordwolf/icestorm/archive/816c47ce83e330d0b293cd5e0a2e82885fb9f74e.zip"
+  sha256 "05db23066a96b233b20ec9c0919066e8accb4ca3dea7367d301b9c403a46bf3e"
 
   depends_on "python3"
   depends_on "pkg-config"
