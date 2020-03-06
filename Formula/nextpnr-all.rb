f42782de5aa93522ae4f29b95fd96c06f430a28c
Esemwy <github.5.smy@neverbox.com>
2020-03-05 21:09:34 -0500

    add nextpnr

---
+++ Formula/nextpnr-all.rb
@@ -0,0 +1,21 @@
+class NextpnrAll < Formula
+  desc "portable FPGA place-and-route tool"
+  homepage "https://github.com/YosysHQ/nextpnr"
+  head "https://github.com/YosysHQ/nextpnr.git"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "eigen" => :build
+  depends_on "python"
+  depends_on "boost"
+  depends_on "boost-python3"
+  depends_on "qt5"
+  depends_on "project-trellis"
+
+  def install
+    system "cmake", "-DARCH=all", "-DTRELLIS_ROOT=#{HOMEBREW_PREFIX}/share/trellis", \
+       ".", *std_cmake_args, "-DBoost_NO_BOOST_CMAKE=on", "-DBUILD_TESTS=OFF", "-DBUILD_GUI=OFF"
+    system "make", "install"
+  end
+
+end
