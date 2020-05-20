0fcc0dff1c2e9640b76fd19ee08036ea05c6870c
Esemwy <github.5.smy@neverbox.com>
2020-05-20 09:20:27 -0400

    Add "openfpgaloader"

---
+++ Formula/openfpgaloader.rb
@@ -0,0 +1,15 @@
+class OpenFPGALoader < Formula
+  desc "JTAG loader for various FPGA"
+  homepage "https://github.com/trabucayre/openFPGALoader"
+  head "https://github.com/trabucayre/openFPGALoader.git"
+
+  depends_on "cmake" => :build
+  depends_on "libftdi" => :build
+  depends_on "argp-standalone" => :build
+
+  def install
+    system "cmake", "-DENABLE_UDEV=OFF", "-DBUILD_STATIC=OFF", "-DLIBARGPSTATIC=/usr/local/lib/libargp.a"
+    system "make", "install"
+  end
+
+end
